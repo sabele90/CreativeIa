4a535562ad750bfe027b2ba3646fcd5642e7a81d
sabele90 <sabele@hotmail.es>
2024-10-30 09:58:23 +0000

    cambios config

--- config/generatedText.php
+++ config/generatedText.php
@@ -4,48 +4,48 @@ return [
     'responses' => [
         'low' => [
             'short' => [
-                "es una espuesta breve y directa.",
-                "es una respuesta corta y poco creativa.",
-                "es una respuesta rápida y sin rodeos.",
+                ",es una espuesta breve y directa.",
+                ",es una respuesta corta y poco creativa.",
+                ",es una respuesta rápida y sin rodeos.",
             ],
             'medium' => [
-                "esta es una respuesta de longitud media, con calidad media de creatividad.",
-                "respuesta balanceada en detalle y simplicidad.",
-                "es muy directa pero con un poco más de contexto, aún te falta creatividad.",
+                ",esta es una respuesta de longitud media, con calidad media de creatividad.",
+                ",respuesta balanceada en detalle y simplicidad.",
+                ",es muy directa pero con un poco más de contexto, aún te falta creatividad.",
             ],
             'long' => [
-                "esta es una respuesta detallada y completa, pero bastante predecible.",
-                "ofrece una explicación más extensa y detallada.",
-                "es un exto extenso pero aún te falla la creatividad.",
+                ",esta es una respuesta detallada y completa, pero bastante predecible.",
+                ",ofrece una explicación más extensa y detallada.",
+                ",es un exto extenso pero aún te falla la creatividad.",
             ],
         ],
         'medium' => [
             'short' => [
-                "es una respuesta breve pero interesante.",
-                "es muy directa con un toque creativo.",
-                "es breve pero con cierta creatividad.",
+                ",es una respuesta breve pero interesante.",
+                ",es muy directa con un toque creativo.",
+                ",es breve pero con cierta creatividad.",
             ],
             'medium' => [
-                "es una respuesta de longitud media y con equilibrio entre claridad y creatividad.",
-                "es una respuesta interesante y con algo de detalle.",
-                "es un texto que ofrece una explicación equilibrada , que tal si aumentas la creatividad?.",
+                ",es una respuesta de longitud media y con equilibrio entre claridad y creatividad.",
+                ",es una respuesta interesante y con algo de detalle.",
+                ",es un texto que ofrece una explicación equilibrada , que tal si aumentas la creatividad?.",
             ],
             'long' => [
-                "esta es una respuesta larga con un buen nivel de detalle e interés.",
-                "es un texto extenso que ofrece una buena cantidad de contexto e interés.",
-                "es una respuesta completa y un poco creativa.",
+                ", es una respuesta larga con un buen nivel de detalle e interés.",
+                ",es un texto extenso que ofrece una buena cantidad de contexto e interés.",
+                ",es una respuesta completa y un poco creativa.",
             ],
         ],
         'high' => [
             'short' => [
-                "es una respuesta breve, pero muy creativa.",
-                "mucha creatividad en pocas palabras.",
-                "es muy corta pero demasiado creativa.",
+                ",es una respuesta breve, pero muy creativa.",
+                ",mucha creatividad en pocas palabras.",
+                ",es muy corta pero demasiado creativa.",
             ],
             'medium' => [
-                "Texto de longitud media con bastante creatividad.",
-                "Respuesta interesante y única en contenido.",
-                "Texto que añade creatividad sin perder enfoque.",
+                ",es un texto de longitud media con bastante creatividad.",
+                ", una respuesta interesante y única en contenido.",
+                ", es un texto que añade creatividad sin perder enfoque.",
             ],
             'long' => [
                 " ,una respuesta larga, muy creativa y única.",
